105d78f5e10ee5620656f5e45e7a02dae190c864
John Spackman <john.spackman@zenesis.com>
2021-01-23 17:25:39 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Qooxdoo Widget Browser
+
+A GUI framework application to show all widgets with different themes
+
+## Online version
+
+http://www.qooxdoo.org/qxl.widgetbrowser/
+
+## Developing widgetbrowser
+Clone this repo and compile it:
+
+```
+    $ git clone https://github.com/qooxdoo/qxl.widgetbrowser
+    $ cd qxl.widgetbrowser
+    $ qx pkg install
+    $ qx serve
+```
+Then open [http://localhost:8080](http://localhost:8080)
+
---
+++ source/class/qxl/testrunner/TestLoader.js
@@ -0,0 +1,55 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("qxl.testrunner.TestLoader",
+{
+  extend : qx.dev.unit.TestLoader,
+
+  /*
+  *****************************************************************************
+     STATICS
+  *****************************************************************************
+  */
+
+  statics :
+  {
+    /**
+     * Returns the TestLoader instance
+     *
+     * @return {qxl.testrunner.TestLoader} TestLoader instance
+     */
+    getInstance : function() {
+      return this.instance;
+    }
+  },
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    main : function()
+    {
+      qxl.testrunner.TestLoader.instance = this;
+      this.base(arguments);
+    }
+  }
+});
